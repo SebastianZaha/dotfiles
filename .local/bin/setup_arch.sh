0986e6ac8d2d530db1b648d1137c55d272ab0248
Sebastian Zaha <sebastian.zaha@gmail.com>
2022-07-06 19:52:56 +0200

    arch

--- .local/bin/setup_arch.sh
+++ .local/bin/setup_arch.sh
@@ -1,8 +1,9 @@
 pacman -S --needed \
     pkgstats \ 
     neovim screen git wget openssl openssh mc man sudo htop lsof \ 
-    base-devel ripgrep lazygit go \
-    wayland firefox foot bemenu-wayland noto-fonts-emoji noto-fonts noto-fonts-extra \ 
+    base-devel ripgrep lazygit go cmake \
+    wayland firefox foot bemenu-wayland \
+    noto-fonts-emoji noto-fonts noto-fonts-extra noto-fonts-cjk \ 
     ttf-jetbrains-mono \
     sway waybar swappy grim slurp \
     pulseaudio pavucontrol
